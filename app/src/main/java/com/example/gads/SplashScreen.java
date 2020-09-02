17aafe6158bebb56eb30126437f341d688f6a487
Elly Wenani <ellywenani@gmail.com>
2020-09-02 13:18:01 +0300

    coded splashactivity to have animation before switching to homeactivity

--- app/src/main/java/com/example/gads/SplashScreen.java
+++ app/src/main/java/com/example/gads/SplashScreen.java
@@ -1,5 +1,6 @@
 package com.example.gads;
 
+import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Bundle;
 
@@ -8,41 +9,60 @@ import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.util.Pair;
 
+import android.os.Handler;
 import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class SplashScreen extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN = 3000;
+
+    //Variables
+    private ImageView image;
+    private Animation topAnim;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
+        //Animations
+        topAnim = AnimationUtils.loadAnimation(this, R.anim.top_anim);
+
+        //Hooks
+        image = findViewById(R.id.img_load);
+
+        image.setAnimation(topAnim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent home = new Intent(SplashScreen.this, HomeActivity.class);
+
+                Pair[] pairs = new Pair[1];
+                pairs[0] = new Pair<View, String>(image, "logo_image");
+
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+                    ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(SplashScreen.this);
+                    startActivity(home, options.toBundle());
+                    SplashScreen.this.finish();
+                }
+            }
+        }, SPLASH_SCREEN);
 
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    public void onBackPressed() {
+        //Nothing happens when back button is pressed
     }
 }
