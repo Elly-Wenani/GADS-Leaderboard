7283a79467ac48c0415bb5c6326c261a8dea011a
Elly Wenani <ellywenani@gmail.com>
2020-09-02 17:04:08 +0300

    Coded submit xml to have a logo and back arrow in toolbar

--- app/src/main/java/com/example/gads/SubmitActivity.java
+++ app/src/main/java/com/example/gads/SubmitActivity.java
@@ -16,7 +16,5 @@ public class SubmitActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_submit);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
     }
 }
