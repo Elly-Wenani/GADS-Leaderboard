eb53d32c9a53bda0939b9d80497dc11243e0d2ee
Elly Wenani <ellywenani@gmail.com>
2020-09-08 13:27:52 +0300

    Created ServiceBuilder to handle the base URL

---
+++ app/src/main/java/com/example/gads/Services/ServiceBuilder.java
@@ -0,0 +1,17 @@
+package com.example.gads.Services;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ServiceBuilder {
+
+    private static final String URL = "https://gadsapi.herokuapp.com/";
+    private static Retrofit.Builder sBuilder = new Retrofit.Builder().baseUrl(URL)
+            .addConverterFactory(GsonConverterFactory.create());
+
+    private static Retrofit sRetrofit = sBuilder.build();
+
+    private static <S> S builderService(Class<S> serviceType){
+        return sRetrofit.create(serviceType);
+    }
+}
