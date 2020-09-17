26c547e55dedb6cfe5fd01f81ec9247e8d2819b5
Elly <ellywenani@gmail.com>
2020-09-17 17:58:32 +0300

    Created project submission service interface

---
+++ app/src/main/java/com/example/gads/Services/ProjectSubmissionService.java
@@ -0,0 +1,22 @@
+package com.example.gads.Services;
+
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+
+public interface ProjectSubmissionService {
+    String SUBMIT_PROJECT_URL
+            = "https://docs.google.com/forms/d/e/"
+            + "1FAIpQLSf9d1TcNU6zc6KR8bSEM41Z1g1zl35cwZr2xyjIhaMAz8WChQ/formResponse";
+
+    @FormUrlEncoded
+    @POST(SUBMIT_PROJECT_URL)
+    Call<Void> submitProject(
+            @Field("entry.1877115667")String firstName,
+            @Field("entry.2006916086")String lastName,
+            @Field("entry.1824927963")String emailAddress,
+            @Field("entry.284483984")String linkToProject
+
+    );
+}
